43d41ed950c133776471e0487ff8d1eec1a4c7cf
Ian Lessing <ilessing@library.ucsb.edu>
2020-12-03 15:13:04 -0800

    try again install knitr and other packages

--- temp.r
+++ temp.r
@@ -1,3 +1,8 @@
+
+workshop_pkg <- c("remotes", "knitr", "rmarkdown", "bookdown", "DT", "tidyverse", "leaflet", "captioner")
+install.packages(workshop_pkg)
+
+
 fileConn<-file("deletethis.txt")
 writeLines(as.character(Sys.time()), fileConn)
 close(fileConn)
